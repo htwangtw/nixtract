ecf55aaa620d6f172655723d59327d4867678403
Dan Gale <danjgale@gmail.com>
2021-04-09 17:36:08 -0400

    fix typo in load confounds kwargs

--- nixtract/cli/nifti.py
+++ nixtract/cli/nifti.py
@@ -226,7 +226,7 @@ def extract_nifti(input_file, roi_file, regressor_file, params):
     )
     if regressor_file is not None:
         extractor.set_regressors(regressor_file, params['regressors'], 
-                                 params["load_confound_kwargs"])
+                                 params["load_confounds_kwargs"])
 
     if (params['discard_scans'] is not None) and (params['discard_scans'] > 0):
         extractor.discard_scans(params['discard_scans'])
